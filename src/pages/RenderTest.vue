3b9617eb44e6e6489b84a2ceaa8810947d36c008
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-03-21 20:40:33 +0100

    add new test page for render fundtion

---
+++ src/pages/RenderTest.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="container">
+    <h1>Render Test</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import { h } from 'vue';
+
+export default {
+  name: 'RenderTest',
+  setup() {
+    return {};
+  },
+};
+</script>
